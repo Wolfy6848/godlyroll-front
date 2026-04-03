07e98c9e6c89d2888d7ecb8fcd175dea686dc408
Wolfy6848 <89296427+Wolfy6848@users.noreply.github.com>
2026-04-03 23:59:01 +0100

    Update Navbar.jsx

--- client/src/components/app/Navbar.jsx
+++ client/src/components/app/Navbar.jsx
@@ -1,28 +1,23 @@
 import React from "react";
-import { AppBar, Toolbar, IconButton, Typography } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/logo.png"; // your logo image import
+import UserIcon from "../../assets/user-icon.svg"; // user icon image
 
 const Navbar = ({ toggleChat }) => {
   return (
-    <AppBar
-      position="sticky"
-      sx={{
-        backgroundColor: "#14151D",
-        zIndex: 1300,
-        boxShadow: "none",
-        borderBottom: "1px solid #282A3A",
-      }}
-    >
-      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-        <Typography variant="h6" component="div">
-          FullSend
-        </Typography>
-
-        <IconButton color="inherit" onClick={toggleChat}>
-          <MenuIcon />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+    <nav className="navbar">
+      <Link to="/home" className="navbar-logo">
+        <img src={Logo} alt="Logo" height="40" />
+      </Link>
+      <ul className="navbar-menu">
+        <li><Link to="/profile">Profile</Link></li>
+        <li><Link to="/leaderboard">Leaderboard</Link></li>
+        <li><button onClick={toggleChat}>Chat</button></li>
+      </ul>
+      <div className="navbar-user">
+        <img src={UserIcon} alt="User" height="30" />
+      </div>
+    </nav>
   );
 };
 
